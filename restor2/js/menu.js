833a480e80fae26720cc2b1cdb5dea49d6bd6865
AlexeyTcapaev <lexx1995@mail.ru>
2017-04-24 22:40:35 +0300

    24.04.17

--- restor2/js/menu.js
+++ restor2/js/menu.js
@@ -1,43 +1,65 @@
 $(function()
 {
-    var i=0;
+  var log_off=$.preload("../img-main/logo/radio-on-button.svg");
+    console.log(log_off);
+    var i=1;
      var j=0;
      var k=0;
      var f=0;
     
    
-     $('.check1').click(function()
+     $('#p1l').click(function()
     {    
+
          i++;
         
          if((i%2)==1)
-                 document.getElementById("select1").style.backgroundImage="url(../img-main/logo/radio-on-button.svg)";
+                 {  
+                    document.getElementById("select1").style.backgroundImage="url(../img-main/logo/radio-on-button.svg)";
+                      for(var a=3;a<document.getElementById("cont").childNodes.length;a+=2)
+                       {
+                            $(document.getElementById("cont").childNodes[a]).fadeIn();
+                           
+                        }
+                       
+                 }
           if((i%2)==0)
-              document.getElementById("select1").style.backgroundImage="url(../img-main/logo/radio-off-button.svg)";
+              {
+                    document.getElementById("select1").style.backgroundImage="url(../img-main/logo/radio-off-button.svg)";
+                    for(var b=3;b<document.getElementById("cont").childNodes.length;b+=2)
+                        {
+                            $(document.getElementById("cont").childNodes[b]).fadeOut();
+                        }    
+              }
     });
-    $('.check2').click(function()
+      $('#p2l').click(function()
     {    
          j++;
+        
          if((j%2)==1)
                  document.getElementById("select2").style.backgroundImage="url(../img-main/logo/radio-on-button.svg)";
           if((j%2)==0)
               document.getElementById("select2").style.backgroundImage="url(../img-main/logo/radio-off-button.svg)";
     });
-    $('.check3').click(function()
+    $('#p3l').click(function()
     {    
          k++;
+        
          if((k%2)==1)
                  document.getElementById("select3").style.backgroundImage="url(../img-main/logo/radio-on-button.svg)";
           if((k%2)==0)
               document.getElementById("select3").style.backgroundImage="url(../img-main/logo/radio-off-button.svg)";
     });
-    $('.check4').click(function()
+     $('#p4l').click(function()
     {    
          f++;
+        
          if((f%2)==1)
                  document.getElementById("select4").style.backgroundImage="url(../img-main/logo/radio-on-button.svg)";
           if((f%2)==0)
               document.getElementById("select4").style.backgroundImage="url(../img-main/logo/radio-off-button.svg)";
     });
+
+
 });
     
