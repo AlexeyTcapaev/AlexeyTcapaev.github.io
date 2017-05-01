9f3fc66e6cc379ad44d3dff012de4326792883f4
AlexeyTcapaev <lexx1995@mail.ru>
2017-05-01 21:30:08 +0300

    21.28

--- restor2/js/window.js
+++ restor2/js/window.js
@@ -1,31 +1,43 @@
 $(document).ready(function(){
-     document.getElementById("img_w").width=document.getElementById("img_t").width;
-    document.getElementById("img_w").height=document.getElementById("img_t").height;
-    console.log(document.getElementById("img_t").width);
-     console.log(document.getElementById("img_t").height);
-     console.log(document.getElementById("img_w").width);
-     console.log(document.getElementById("img_w").height);
-   var canvas = document.getElementById('img_w').getContext('2d');
-   
-    // создание изображения
-    var img = new Image();
-    img.src = 'img-main/window/sobor2.png';
-    
-      var img1 = new Image();
-        img1.src = 'img-main/window/sobor3.png';
-    // копируем изображение в canvas
-    $(img).load(function(){
-        
-      canvas.drawImage(img,0,0);
-    });
- 
-    function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
+      function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
     function toHex(n) {
       n = parseInt(n,10);
       if (isNaN(n)) return "00";
       n = Math.max(0,Math.min(n,255));
       return "0123456789ABCDEF".charAt((n-n%16)/16)  + "0123456789ABCDEF".charAt(n%16);
     }
+   
+    console.log(document.getElementById("img_t").width);
+     console.log(document.getElementById("img_t").height);
+     console.log(document.getElementById("img_w").width);
+     console.log(document.getElementById("img_w").height);
+   var canvas = document.getElementById('img_w').getContext('2d'); 
+    var canvas1 = document.getElementById('img_t').getContext('2d');
+   
+    // создание изображения
+    var img = new Image();
+    img.src = 'img-main/window/sobor2.png';
+       document.getElementById("img_t").width=img.width;
+        document.getElementById("img_t").height=img.height;
+    console.log(document.getElementById("img_t").width)
+      var img1 = new Image();
+        img1.src = 'img-main/window/sobor3.png';
+    document.getElementById("img_w").width=img1.width;
+        document.getElementById("img_w").height=img1.height;
+    // копируем изображение в canvas
+    $(img).load(function(){
+        
+      canvas.drawImage(img,0,0);
+      
+    });
+     $(img1).load(function(){
+        
+      canvas1.drawImage(img1,0,0);
+      
+    });
+ 
+ 
+   
     $('#img_t').click(function(event){
       // получение координат
       var x = event.offsetX==undefined?event.layerX:event.offsetX;
