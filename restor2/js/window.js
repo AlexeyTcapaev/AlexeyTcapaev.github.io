520a5223c828e0a9a8b04ec88d794e35daf1babe
AlexeyTcapaev <lexx1995@mail.ru>
2017-05-03 12:38:03 +0300

    03.05

--- restor2/js/window.js
+++ restor2/js/window.js
@@ -10,14 +10,22 @@ $(document).ready(function(){
       n = Math.max(0,Math.min(n,255));
       return "0123456789ABCDEF".charAt((n-n%16)/16)  + "0123456789ABCDEF".charAt(n%16);
     }
+    
      var var_i1 = document.getElementById('img_t'),
         ctx1     = var_i1.getContext('2d'),
         pic1     = new Image();
+            if(screen.width==1366)
+                {
+                     var_i1.width = 1300;
+                     var_i1.height =  650;
+                    pic1.src = '/restor2/img-main/window/sobor3_1300_650.png';
+                }
+     if(screen.width==1920)
+         {
           var_i1.width = 1900;
           var_i1.height =  960;
-            console.log(var_i1.width);
-            console.log(var_i1.height);
           pic1.src = '/restor2/img-main/window/sobor3.png';
+         }
     pic1.onload = function ()
     {
         ctx1.drawImage(pic1, 0, 0);  
@@ -25,11 +33,19 @@ $(document).ready(function(){
      var var_i2 = document.getElementById('img_w'),
         ctx2     = var_i2.getContext('2d'),
         pic2     = new Image();
+    if(screen.width==1366)
+                {
+                     var_i2.width = 1300;
+                     var_i2.height =  650;
+                    pic2.src = '/restor2/img-main/window/sobor2_1300_650.png';
+                }
+    
+    if(screen.width==1920)
+        {
           var_i2.width = 1900;
           var_i2.height =  960;
-            console.log(var_i2.width);
-            console.log(var_i2.height);
           pic2.src = '/restor2/img-main/window/sobor2.png';
+        }
     pic2.onload = function ()
     {
         ctx2.drawImage(pic2, 0, 0);  
