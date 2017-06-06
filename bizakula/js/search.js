75da5872f64164ec4d0d2fe19eb458c98ed73ebc
AlexeyTcapaev <lexx1995@mail.ru>
2017-06-06 19:42:09 +0300

    314

--- bizakula/js/search.js
+++ bizakula/js/search.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         document.getElementById("s").style.background="white";
        document.getElementById("s").style.color="#202e48";
        document.getElementById("head_search_form").style.background="white";
-         document.getElementById("s_b").style.background="url('/content/search_2.svg')";
+         document.getElementById("s_b").style.background="url('content/search_2.svg')";
        
     });
      $('body').click(function(event){
@@ -14,7 +14,7 @@ $(document).ready(function(){
                 document.getElementById("s").style.background="#192438";
                document.getElementById("s").style.color="rgba(255,255,255,1)";
                    document.getElementById("head_search_form").style.background="#192438";
-                  document.getElementById("s_b").style.background="url('/content/search_1.svg')";
+                  document.getElementById("s_b").style.background="url('content/search_1.svg')";
                    document.getElementById("s_b").style.backgroundRepeat="no-repeat";
                   document.getElementById("s_b").style.backgroundSize="contain";
              }
