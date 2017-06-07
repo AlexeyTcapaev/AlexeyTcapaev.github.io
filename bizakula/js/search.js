537de9ef1c864c87b543a4e68871364dd3ae2d87
AlexeyTcapaev <lexx1995@mail.ru>
2017-06-07 22:22:31 +0300

    2

--- bizakula/js/search.js
+++ bizakula/js/search.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     
-    $('.search').click(function(event){
+     document.getElementById("s_b").style.background="url('content/search_1.svg')";
+     document.getElementById("s_b").style.backgroundRepeat="no-repeat";
+     document.getElementById("s_b").style.backgroundSize="contain";
+     $('.search').click(function(event){
         document.getElementById("s").style.background="white";
        document.getElementById("s").style.color="#202e48";
        document.getElementById("head_search_form").style.background="white";
          document.getElementById("s_b").style.background="url('content/search_2.svg')";
+          document.getElementById("s_b").style.backgroundRepeat="no-repeat";
+                  document.getElementById("s_b").style.backgroundSize="contain";
        
     });
      $('body').click(function(event){
@@ -20,5 +25,9 @@ $(document).ready(function(){
              }
        
     });
+    particlesJS.load('particles-js','/js/particles/particles.json',function(){
+        console.log('callback - particle-js')
+    })
+     
     
 });
